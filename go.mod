e6bc30d47bba0a9b6e5b4e4235b6ad3b814f6022
Jeff Huter <slothninja2@gmail.com>
2021-01-26 19:44:18 -0500

    added NewClient support; moved secrets to cookie module

--- go.mod
+++ go.mod
@@ -5,11 +5,11 @@ go 1.14
 require (
 	cloud.google.com/go/datastore v1.4.0
 	cloud.google.com/go/logging v1.1.2
-	github.com/SlothNinja/log v1.0.1
-	github.com/SlothNinja/sn v1.0.2
-	github.com/SlothNinja/user-controller v1.0.15
+	github.com/SlothNinja/cookie v1.0.1
+	github.com/SlothNinja/log v1.0.2
+	github.com/SlothNinja/sn v1.0.3
+	github.com/SlothNinja/user-controller v1.0.17
 	github.com/gin-contrib/sessions v0.0.3
 	github.com/gin-gonic/gin v1.6.3
-	github.com/gorilla/securecookie v1.1.1
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 )
--- main.go
+++ main.go
@@ -2,29 +2,26 @@ package main
 
 import (
 	"context"
-	"encoding/base64"
-	"fmt"
 	"net/http"
 	"os"
 	"time"
 
 	"cloud.google.com/go/datastore"
 	"cloud.google.com/go/logging"
+	"github.com/SlothNinja/cookie"
 	"github.com/SlothNinja/log"
 	"github.com/SlothNinja/sn"
 	ucon "github.com/SlothNinja/user-controller"
 	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/securecookie"
 	"github.com/patrickmn/go-cache"
 )
 
 const (
-	hashKeyLength        = 64
-	blockKeyLength       = 32
-	sessionName          = "sng-oauth"
-	GOOGLE_CLOUD_PROJECT = "GOOGLE_CLOUD_PROJECT"
+	hashKeyLength      = 64
+	blockKeyLength     = 32
+	sessionName        = "sng-oauth"
+	googleCloudProject = "GOOGLE_CLOUD_PROJECT"
 )
 
 func main() {
@@ -35,15 +32,15 @@ func main() {
 
 	logger := logClient.Logger("user-service")
 
-	client := sn.NewClient(newDSClient(), logger, cache.New(30*time.Minute, 10*time.Minute), gin.New())
+	client := sn.NewClient(newDSClient(logger), logger, cache.New(30*time.Minute, 10*time.Minute), gin.New())
 
-	s, err := getSecrets()
+	store, err := cookie.NewClient(client.Log, client.Cache).NewStore()
 	if err != nil {
-		panic(err.Error())
+		logger.Panicf("unable create cookie store: %v", err)
 	}
 
 	client.Router.Use(
-		sessions.Sessions(sessionName, createCookieStore(s)),
+		sessions.Sessions(sessionName, store),
 		gin.LoggerWithWriter(logger.StandardLogger(logging.Debug).Writer()),
 		gin.RecoveryWithWriter(logger.StandardLogger(logging.Critical).Writer()),
 	)
@@ -51,103 +48,11 @@ func main() {
 	// user controller
 	ucon.NewClient(client.DS, logger, client.Cache, client.Router)
 
-	// cookie route
-	client.Router.GET("cookie", cookieHandler(s))
-
 	// warmup
 	client.Router.GET("_ah/warmup", func(c *gin.Context) { c.Status(http.StatusOK) })
 
 	client.Router = staticRoutes(client.Router)
 	client.Router.Run()
-}
-
-// type client struct {
-// 	dsClient  *datastore.Client
-// 	logClient *log.Client
-// 	mcache    *cache.Cache
-// 	router    *gin.Engine
-// }
-//
-// func newClient() *client {
-// 	return &client{
-// 		dsClient:  newDSClient(),
-// 		logClient: newLogClient(),
-// 		mcache:    cache.New(30*time.Minute, 10*time.Minute),
-// 		router:    gin.Default(),
-// 	}
-// }
-
-type secrets struct {
-	HashKey   []byte         `json:"hashKey"`
-	BlockKey  []byte         `json:"blockKey"`
-	UpdatedAt time.Time      `json:"updatedAt"`
-	Key       *datastore.Key `datastore:"__key__" json:"-"`
-}
-
-func getSecrets() (secrets, error) {
-	s := secrets{
-		Key: secretsKey(),
-	}
-
-	c := context.Background()
-	dsClient, err := datastore.NewClient(c, "")
-	if err != nil {
-		return s, err
-	}
-
-	err = dsClient.Get(c, s.Key, &s)
-	if err == nil {
-		return s, nil
-	}
-
-	if err != datastore.ErrNoSuchEntity {
-		return s, err
-	}
-
-	log.Warningf("generated new secrets")
-	s, err = genSecrets()
-	if err != nil {
-		return s, err
-	}
-
-	_, err = dsClient.Put(c, s.Key, &s)
-	return s, err
-}
-
-func secretsKey() *datastore.Key {
-	return datastore.NameKey("Secrets", "root", nil)
-}
-
-func genSecrets() (secrets, error) {
-	s := secrets{
-		HashKey:  securecookie.GenerateRandomKey(hashKeyLength),
-		BlockKey: securecookie.GenerateRandomKey(blockKeyLength),
-		Key:      secretsKey(),
-	}
-
-	if s.HashKey == nil {
-		return s, fmt.Errorf("generated hashKey was nil")
-	}
-
-	if s.BlockKey == nil {
-		return s, fmt.Errorf("generated blockKey was nil")
-	}
-
-	return s, nil
-}
-
-func (s *secrets) Load(ps []datastore.Property) error {
-	return datastore.LoadStruct(s, ps)
-}
-
-func (s *secrets) Save() ([]datastore.Property, error) {
-	s.UpdatedAt = time.Now()
-	return datastore.SaveStruct(s)
-}
-
-func (s *secrets) LoadKey(k *datastore.Key) error {
-	s.Key = k
-	return nil
 }
 
 // staticHandler for local development since app.yaml is ignored
@@ -165,36 +70,6 @@ func staticRoutes(r *gin.Engine) *gin.Engine {
 	return r
 }
 
-func createCookieStore(s secrets) cookie.Store {
-	if !sn.IsProduction() {
-		log.Debugf("hashKey: %s\nblockKey: %s",
-			base64.StdEncoding.EncodeToString(s.HashKey),
-			base64.StdEncoding.EncodeToString(s.BlockKey),
-		)
-		store := cookie.NewStore(s.HashKey, s.BlockKey)
-		opts := sessions.Options{
-			Domain: "fake-slothninja.com",
-			Path:   "/",
-		}
-		store.Options(opts)
-		return store
-	}
-	store := cookie.NewStore(s.HashKey, s.BlockKey)
-	opts := sessions.Options{
-		Domain: "slothninja.com",
-		Path:   "/",
-		Secure: true,
-	}
-	store.Options(opts)
-	return store
-}
-
-func cookieHandler(s secrets) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.JSON(http.StatusOK, s)
-	}
-}
-
 func setGinMode() {
 	if sn.IsProduction() {
 		gin.SetMode(gin.ReleaseMode)
@@ -204,13 +79,13 @@ func setGinMode() {
 }
 
 func getProjectID() string {
-	return os.Getenv(GOOGLE_CLOUD_PROJECT)
+	return os.Getenv(googleCloudProject)
 }
 
-func newDSClient() *datastore.Client {
+func newDSClient(log *log.Logger) *datastore.Client {
 	client, err := datastore.NewClient(context.Background(), "")
 	if err != nil {
-		log.Panicf("unable to create datastore client: %w", err)
+		log.Panicf("unable to create datastore client: %v", err)
 	}
 	return client
 }
@@ -218,7 +93,7 @@ func newDSClient() *datastore.Client {
 func newLogClient() *log.Client {
 	client, err := log.NewClient(getProjectID())
 	if err != nil {
-		log.Panicf("unable to create logging client: %w", err)
+		log.Panicf("unable to create logging client: %v", err)
 	}
 	return client
 }
