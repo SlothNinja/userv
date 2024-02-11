e94ba49fc37e0281c88c35055c60a8c52e112b1b
Jeff Huter <slothninja2@gmail.com>
2024-02-11 13:54:08 -0500

    update modules, simplified current user in frontend

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,14 @@
 <script setup>
 /////////////////////////////////////////////////////
 // get and provide current user
-import { computed, provide, unref, ref, watch } from 'vue'
+import { computed, provide, unref } from 'vue'
 import  { useFetch } from '@/snvue/fetch'
 import { cuKey } from '@/composables/keys'
 import _get from 'lodash/get'
 
 const cuURL = `${import.meta.env.VITE_USER_BACKEND}sn/user/current`
 const { data } = useFetch(cuURL).json()
-
-const cu = ref({})
-watch(data, () => { cu.value = _get(unref(data), 'CU', {})})
+const cu = computed(() => _get(unref(data), 'CU', {}))
 
 function updateCU(user) {
   cu.value = unref(user)
